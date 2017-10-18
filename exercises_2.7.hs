1ac32046bec5f1bf8a50d8e6576c09af5af69383
Craige McWhirter <craige@mcwhirter.io>
2017-10-18 18:20:52 +1000

    2.7 Declaring values

---
+++ exercises_2.7.hs
@@ -0,0 +1,18 @@
+{- Exercises: Heal the Sick
+The following code samples are broken and won’t compile. The first
+two are as you might enter into the REPL; the third is from a source
+file. Find the mistakes and fix them so that they will.  -}
+
+-- 1. let area x = 3. 14 * (x * x)
+let area x = 3.14 * (x * x)
+
+-- 2. let double x = b * 2
+let double x = x * 2
+
+{- 3. x = 7
+       y = 10
+      f = x + y
+-}
+x = 7
+y = 10
+f = x + y
