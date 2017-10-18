ae2b5632c859b1da9583f1324c6e80ac3faaf7d7
Craige McWhirter <craige@mcwhirter.io>
2017-10-18 17:21:30 +1000

    2.5 Exercises: Comprehension Check

---
+++ comprehensionCheck_2.5.hs
@@ -0,0 +1,8 @@
+{- Write one function that has one parameter and works for all the following expressions. Be sure to name the function.
+3.14 * (5 * 5)
+3.14 * (10 * 10)
+3.14 * (2 * 2)
+3.14 * (4 * 4)
+-}
+
+shortPi x = 3.14 * x
